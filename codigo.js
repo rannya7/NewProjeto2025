9d581a43edae5e94ede7f50fc77f621a2cbfe945
Rannya Denise <rannya.denise@escolar.ifrn.edu.br>
2024-12-15 14:17:48 -0300

    Update codigo.js

---
+++ codigo.js
@@ -0,0 +1,14 @@
+function mostrar(){
+    senha.setAttribute("type", "text");
+    
+}
+
+
+
+mostrarsenha.addEventListener("click", mostrar);
+
+function esconder(){
+    senha.setAttribute("type", "password");
+}
+
+escondersenha.addEventListener("click", esconder);
